164296cb6b39c9dc148b9ca201109ef967104739
prasannaboligala234 <prasanna.boligala@capgemini.com>
2023-11-08 13:06:53 +0530

    Admin and users entity Created By Prasanna

--- src/main/java/com/cbp/in/entity/Admin.java
+++ src/main/java/com/cbp/in/entity/Admin.java
@@ -1,5 +1,51 @@
 package com.cbp.in.entity;
 
-public class Admin {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+	@Id
+	private String adminId;
+	@OneToOne(cascade = CascadeType.ALL)
+	private Users users;
+
+	public Admin() {
+	
+	}
+
+	public Admin(String adminId, Users users) {
+
+		this.adminId = adminId;
+		this.users = users;
+	}
+
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+
+	public Users getUsers() {
+		return users;
+	}
+
+	public void setUsers(Users users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", users=" + users + "]";
+	}
 }
