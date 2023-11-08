8e5b079ea3e200eaa0edb1050257fb976b2098c0
suryav2608 <surya-shyam.vutti@capgemini.com>
2023-11-08 12:01:36 +0530

    credit card and payment entity created

--- src/main/java/com/cbp/in/entity/CreditCard.java
+++ src/main/java/com/cbp/in/entity/CreditCard.java
@@ -1,5 +1,95 @@
 package com.cbp.in.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreditCard {
 
-}
+	
+		@Id
+		private Long id;
+		private String cardName;
+		private String cardType;
+		private String cardNumber;
+		private LocalDate expiryDate;
+		private String bankName;
+		@ManyToOne
+		private Customers customers;
+		public CreditCard() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		public CreditCard(Long id, String cardName, String cardType, String cardNumber, LocalDate expiryDate,
+				String bankName, Customers customers) {
+			super();
+			this.id = id;
+			this.cardName = cardName;
+			this.cardType = cardType;
+			this.cardNumber = cardNumber;
+			this.expiryDate = expiryDate;
+			this.bankName = bankName;
+			this.customers = customers;
+		}
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getCardName() {
+			return cardName;
+		}
+		public void setCardName(String cardName) {
+			this.cardName = cardName;
+		}
+		public String getCardType() {
+			return cardType;
+		}
+		public void setCardType(String cardType) {
+			this.cardType = cardType;
+		}
+		public String getCardNumber() {
+			return cardNumber;
+		}
+		public void setCardNumber(String cardNumber) {
+			this.cardNumber = cardNumber;
+		}
+		public LocalDate getExpiryDate() {
+			return expiryDate;
+		}
+		public void setExpiryDate(LocalDate expiryDate) {
+			this.expiryDate = expiryDate;
+		}
+		public String getBankName() {
+			return bankName;
+		}
+		public void setBankName(String bankName) {
+			this.bankName = bankName;
+		}
+		public Customers getCustomers() {
+			return customers;
+		}
+		public void setCustomers(Customers customers) {
+			this.customers = customers;
+		}
+		@Override
+		public String toString() {
+			return "CreditCard [id=" + id + ", cardName=" + cardName + ", cardType=" + cardType + ", cardNumber="
+					+ cardNumber + ", expiryDate=" + expiryDate + ", bankName=" + bankName + ", customers=" + customers
+					+ "]";
+		}
+		
+
+	 
+	}
--- src/main/java/com/cbp/in/entity/Payment.java
+++ src/main/java/com/cbp/in/entity/Payment.java
@@ -1,5 +1,52 @@
 package com.cbp.in.entity;
 
-public class Payment {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+	@Id
+	private long paymentId;
+	private String method;
+	private double amountDue;
+	public Payment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Payment(long paymentId, String method, double amountDue) {
+		super();
+		this.paymentId = paymentId;
+		this.method = method;
+		this.amountDue = amountDue;
+	}
+	public long getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(long paymentId) {
+		this.paymentId = paymentId;
+	}
+	public String getMethod() {
+		return method;
+	}
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	public double getAmountDue() {
+		return amountDue;
+	}
+	public void setAmountDue(double amountDue) {
+		this.amountDue = amountDue;
+	}
+	@Override
+	public String toString() {
+		return "Payment [paymentId=" + paymentId + ", method=" + method + ", amountDue=" + amountDue + "]";
+	}
+ 
 }
